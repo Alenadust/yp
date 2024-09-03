b0587d42fcfd544a83a55c4f05dcfb54e69b1f68
Alenadar <95030565+Alenadar@users.noreply.github.com>
2024-09-03 10:47:41 +0500

    Create task4.c
    Signed-off-by: Alenadar <95030565+Alenadar@users.noreply.github.com>

---
+++ labs/lab1/task4.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    int a; int b; int c; float x;
+    scanf("%d",&a);
+    scanf("%d",&b);
+    scanf("%d",&c);
+    printf("x = %d",(c-b)/a);
+
+    return 0;
+}
