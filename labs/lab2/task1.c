dae737b87e4ca44c4a3e67485ec74f65cc28f7c9
Alenadar <95030565+Alenadar@users.noreply.github.com>
2024-09-10 10:02:51 +0500

    Update task1.c
    Signed-off-by: Alenadar <95030565+Alenadar@users.noreply.github.com>

--- labs/lab2/task1.c
+++ labs/lab2/task1.c
@@ -1,8 +1,18 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 int main()
 {
-    printf("Hello World");
-
-    return 0;
+    float numbers[5];
+    srand(time(NULL));   
+    for (int i = 0; i <= 5; i++) {
+    numbers[i]=rand();
+    
+}
+    for (int i = 0; i <= 5; i++) {
+    printf("%f \n",numbers[i]);
+    
+}
+return 0;
 }
