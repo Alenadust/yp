52889e271a2c159fdd50446cfc5192ac47a6f124
Alenadar <95030565+Alenadar@users.noreply.github.com>
2024-09-23 21:51:58 +0500

    Update task5.c
    Signed-off-by: Alenadar <95030565+Alenadar@users.noreply.github.com>

--- labs/lab1/task5.c
+++ labs/lab1/task5.c
@@ -2,11 +2,11 @@
 #include <stdlib.h>
 #include <string.h>
 
-int main() {
+int main(int argc, char *argv[]) {
  
     FILE *file;
     char ch;
-    file = fopen("file1.txt", "r");
+    file = fopen(argv[1], "r");
 
     while ((ch = fgetc(file)) != EOF) {
         printf("%c", ch);
