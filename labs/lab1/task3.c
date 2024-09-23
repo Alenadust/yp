4042ab8daa87d6bdafe7c4461ddff3f84da02932
Alenadar <95030565+Alenadar@users.noreply.github.com>
2024-09-23 19:52:45 +0500

    Update task3.c
    Signed-off-by: Alenadar <95030565+Alenadar@users.noreply.github.com>

--- labs/lab1/task3.c
+++ labs/lab1/task3.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
-int main()
-{
-    int a; 
-    scanf("%d",&a);
-    printf("введенное целое число = %d",a);
 
+int main(int argc, char *argv[]) {
+    printf("Количество аргументов: %d\n", argc-1);
+    printf("Аргументы:\n");
+    for (int i = 1; i < argc; i++) {
+        printf("%d: %s\n", i, argv[i]);
+    }
+    
     return 0;
 }
