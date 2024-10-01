22b871897fd902a2e718848c55ecc85ccfccf121
Alenadar <95030565+Alenadar@users.noreply.github.com>
2024-10-01 10:17:36 +0500

    Update task4.c
    Signed-off-by: Alenadar <95030565+Alenadar@users.noreply.github.com>

--- labs/lab2/task4.c
+++ labs/lab2/task4.c
@@ -5,12 +5,12 @@
 int main(int argc, char *argv[])
 {
     int a,arg1,arg2;
-    a = atoi(argv[1]) -1;
+    a = atoi(argv[1]);
     arg1 = atoi(argv[2]);
     arg2 = atoi(argv[3]);
     float numbers[a]; 
     srand(time(NULL));   
-    for (int i = 0; i <= a; i++) {
+    for (int i = 0; i < a; i++) {
     numbers[i]=(rand()%((arg1-1)-arg2)+arg1) + (rand()%((arg1-1)-arg2)+arg1)/(arg2*1.0);
     
 }
